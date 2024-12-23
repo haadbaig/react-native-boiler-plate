6b6ec37af8a09bd1be359037a01686213d855042
haadbaig <haadbaig@gmail.com>
2024-12-24 01:56:56 +0500

    initial setup done. splash screen image issue persist

--- App.tsx
+++ App.tsx
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './src/navigation/MainNavigator';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync(); // Prevent splash screen from auto-hiding
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        // Load any required resources or perform tasks here
+        // await setTimeout(() => {
+        //   console.log("LOGGER---------")
+        // }, 1000)
+        // Simulate a task (e.g., API call)
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setAppIsReady(true); // Mark the app as ready
+      }
+    };
+
+    prepareApp();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // Hide the splash screen once the app is ready
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +64,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  }
 });
